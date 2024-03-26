9e043f8d01d664b563ac9bc5f750b132ca1e85f1
Diego Avila <diego.avila@uned.cr>
2024-03-25 22:36:07 -0600

    wip bitstream generation using vitis

---
+++ mesonbuild/modules/vitis.py
@@ -0,0 +1,60 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2024 The Meson development team
+
+from __future__ import annotations
+import itertools
+import typing as T
+
+from . import ExtensionModule, ModuleReturnValue, ModuleInfo
+from .. import build
+from .. import mesonlib
+from ..interpreter.type_checking import CT_INPUT_KW
+from ..interpreterbase.decorators import KwargInfo, typed_kwargs, typed_pos_args
+
+if T.TYPE_CHECKING:
+    from typing_extensions import TypedDict
+
+    from . import ModuleState
+    from ..interpreter import Interpreter
+    from ..programs import ExternalProgram
+
+    class BitstreamKwargs(TypedDict):
+        sources: T.List[T.Union[mesonlib.FileOrString, build.GeneratedTypes]]
+
+class VitisModule(ExtensionModule):
+    INFO = ModuleInfo('FPGA/Xilinx', '1.4.0', unstable=True)
+
+    def __init__(self, interpreter: Interpreter) -> None:
+        super().__init__(interpreter)
+        self.tools: T.Dict[str, T.Union[ExternalProgram, build.Executable]] = {}
+        self.methods.update({
+            'bitstream': self.bitstream,
+        })
+
+    def _check_tooling(self, state: ModuleState) -> None:
+        self.tools['v++'] = state.find_program('v++')
+
+#todo make project file that uses this args to the example, using the utility libs outside the hello_world dir
+    @typed_pos_args('vitis.project', str,
+                    varargs=(str, mesonlib.File, build.CustomTarget, build.CustomTargetIndex, build.GeneratedList))
+    @typed_kwargs('vitis.project', CT_INPUT_KW.evolve(name='sources'), KwargInfo('user_config_file'
+        (str, mesonlib.File, build.CustomTarget, build.CustomTargetIndex,build.GeneratedList),
+        required=True))
+    def bitstream(self, state: ModuleState,
+                  args: T.Tuple[str, T.List[T.Union[mesonlib.FileOrString, build.GeneratedTypes]]],
+                  kwargs: BitstreamKwargs) -> ModuleReturnValue:
+        if not self.tools:
+            self._check_tooling(state)
+        bitstream_name, arg_sources = args
+        all_sources = self.interpreter.source_strings_to_files(
+            list(itertools.chain(arg_sources, kwargs['sources'])))
+        xclbin_target = build.CustomTarget(
+            F'{bitstream_name}.xclbin',
+            state.subdir,
+            state.subproject,
+            state.environment,
+            [self.tools['v++']],
+            all_sources
+            )
+
+        return ModuleReturnValue(None, [xclbin_target])
